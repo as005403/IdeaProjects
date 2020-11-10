f126bb008e6d24c0f9f35c50ccac82457d5d6c17
Ilya <dobrolinskiy.ilya@mail.ru>
2020-11-11 01:31:10 +0300

    [fix]update project "Huffman archiver"

--- ОИТ/Huffman archiver/src/com/company/Haffman.java
+++ ОИТ/Huffman archiver/src/com/company/Haffman.java
@@ -1,6 +1,5 @@
 package com.company;
 import java.io.*;
-import java.lang.reflect.Array;
 import java.security.SecureRandom;
 import java.util.*;
 
@@ -8,6 +7,54 @@ import java.util.*;
 public class Haffman {
 
     private char[] chars = "abcdefghijklmnopqrstuvwxyz".toCharArray();
+    private ArrayList<Priority> symbolPriorityList = new ArrayList<Priority>();
+    private Queue<Priority> symbolPriorityQueue = new PriorityQueue<>(26, frequencyComparator);
+
+    public Haffman(String fileName){
+        CalculationSymbolsList(fileName);
+        AddSymbolsToQueue();
+    }
+
+    private void CalculationSymbolsList(String fileName) {
+
+        int frequency = 0;
+
+        for (int i = 0; i < chars.length; i++) {
+            Priority temp = new Priority();
+
+            try (FileReader file = new FileReader(fileName)) {
+                int c;
+                while ((c = file.read()) != -1) {
+                    if (chars[i] == (char) c) frequency++;
+                }
+
+                temp.setFrequency(frequency);
+                temp.setSymbol(chars[i]);
+                frequency = 0;
+
+                symbolPriorityList.add(temp);
+            } catch (IOException ex) {
+
+                System.out.println(ex.getMessage());
+            }
+        }
+    }
+    private void AddSymbolsToQueue(){
+
+        //add data to Queue
+        for(int i=0; i<symbolPriorityList.size(); i++){
+            symbolPriorityQueue.add(symbolPriorityList.get(i));
+        }
+    }
+
+    //Анонимный класс компаратора
+    private static Comparator<Priority> frequencyComparator = new Comparator<Priority>(){
+
+            @Override
+            public int compare(Priority c1, Priority c2) {
+                return (int) (c1.getFrequency() - c2.getFrequency());
+            }
+        };
 
     public void generation(){
         try(FileWriter file = new FileWriter("file.txt"))
@@ -25,35 +72,18 @@ public class Haffman {
         }
     }
 
-    public HashMap<Integer, Character> numberOfChars(String fileName) {
-
-        HashMap<Integer, Character> frequency = new HashMap<>();
-        int numberOfRepeats = 0;
-
-        for (int i = 0; i < chars.length; i++) {
-            try (FileReader file = new FileReader(fileName)) {
-                int c;
-                while ((c = file.read()) != -1) {
-                    if (chars[i] == (char) c) numberOfRepeats++;
-                }
-
-
-
-                frequency.put(numberOfRepeats, chars[i]);
-                numberOfRepeats = 0;
-            }
-            catch(IOException ex){
-
-                System.out.println(ex.getMessage());
-            }
+    public void getDataFromList() {
+        for (int i = 0; i < symbolPriorityList.size(); i++) {
+            Priority symbol = symbolPriorityList.get(i);
+            System.out.println(symbol.getSymbol() + ": " + symbol.getFrequency());
         }
+    }
 
-//        HashMap<Character, Integer> frequencySortedByValue = new HashMap<>();
-
-
-
-
-
-        return frequency;
+    public void getDataFromQueue() {
+        while (true) {
+            Priority symbol = symbolPriorityQueue.poll();
+            if (symbol == null) break;
+            System.out.println(symbol.getSymbol() + ": " + symbol.getFrequency());
+        }
     }
 }
--- ОИТ/Huffman archiver/src/com/company/Main.java
+++ ОИТ/Huffman archiver/src/com/company/Main.java
@@ -5,14 +5,9 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
 
-        Haffman haffman = new Haffman();
-        haffman.generation();
-        HashMap<Integer, Character> frequency = haffman.numberOfChars("file.txt");
+        Haffman haffman = new Haffman("file.txt");
 
-        for (Integer chars: frequency.keySet()) {
-
-            System.out.print(chars + ": " + frequency.get(chars));
-            System.out.println();
-        }
+        haffman.getDataFromList();
+//        haffman.getDataFromQueue();
     }
 }
